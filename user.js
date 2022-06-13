bccfebf424a2d6c189c9155b8c23a6c9e2e8d9d1
katsukichi <katsukichi@imagoworks.ai>
2022-06-13 18:41:13 +0900

    test

---
+++ user.js
@@ -0,0 +1,27 @@
+const userSchema = {
+    name: {
+        type: String,
+        default: null,
+    },
+    address: {
+        type: String,
+        default: null,
+    },
+    email: {
+        type: String,
+        default: null,
+    },
+    phoneNumber: {
+        type: String,
+        default: null,
+    },
+    age: {
+        type: Number,
+        default: null,
+    },
+};
+
+
+module.exports(userSchema)
+//하는김에 connection 방식도 바꾸자.
+// factory 패턴 -> 다시 단일 connect를 사용한 패턴으로
